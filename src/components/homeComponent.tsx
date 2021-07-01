7a233f32a332697d544c0bdf3d71e1389c8aa997
Keaton Sentak <ksentak01@gmail.com>
2021-07-01 16:48:36 -0400

    add testimonial component

--- src/components/homeComponent.tsx
+++ src/components/homeComponent.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { Link } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
 import ReactPlayer from 'react-player/lazy';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faQuoteRight, faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
+import Testimonial from './testimonial';
+import RyanRuocco from '../images/RyanRuocco_Testimonial.jpg';
+import TomMcGinnis from '../images/TomMcGinnis.jpg';
 
 const HomeComponent = () => (
 	<div className="HomeComponent">
@@ -36,19 +37,19 @@ const HomeComponent = () => (
 
 			<hr className="separator" />
 
-			<div className="row text-center text-md-none pt-3 pb-3">
+			<div className="row text-center text-md-none py-3">
 				<div className="col-sm-6">
 					<StaticImage
 						src="../images/MM2_Home.jpg"
 						alt="Matt interviewing Adam Silver"
-						className="img-fluid home-img pb-2 pb-sm-none"
+						className="img-fluid home-img mb-2 mb-sm-0"
 					/>
 				</div>
 				<div className="col-sm-6">
 					<StaticImage
 						src="../images/MM3_Home.jpeg"
 						alt="Matt interviewing Adam Silver"
-						className="img-fluid home-img pt-2 pt-sm-none"
+						className="img-fluid home-img mt-2 mt-sm-0"
 					/>
 				</div>
 			</div>
@@ -57,87 +58,27 @@ const HomeComponent = () => (
 
 			{/* Testimonials */}
 			<div className="row">
-				<div className="col-12">
-					<div className="p-20" />
+				<Testimonial
+					testimonialText="Matt Murphy is a talented young play-by-play man. He’s driven,
+          approachable and incredibly skilled. I’m so excited to watch
+          this young man’s career."
+					testimonialAuthor="Ryan Ruocco"
+					testimonialFirm="ESPN, YES Network, DAZN"
+					imgPath={RyanRuocco}
+					imgAlt="Ryan Ruocco"
+				/>
 
-					{/* Testimonial 1 */}
-					<div className="testimonial-item">
-						<div className="testimonial-content">
-							{/* Testimonial Image */}
-							<div className="testimonial-picture">
-								<StaticImage
-									src="../images/RyanRuocco_Testimonial.jpg"
-									alt="Ryan Ruocco"
-									className="img-fluid"
-								/>
-							</div>
-
-							{/* Testimonial Text */}
-							<div className="testimonial-text">
-								<p>
-									Matt Murphy is a talented young play-by-play man. He’s driven,
-									approachable and incredibly skilled. I’m so excited to watch
-									this young man’s career.
-								</p>
-							</div>
-							{/* Author */}
-							<div className="testimonial-author-info">
-								<h5 className="testimonial-author">Ryan Ruocco</h5>
-								<p className="testimonial-firm">ESPN, YES Network, DAZN</p>
-							</div>
-
-							<div className="testimonial-icon">
-								<FontAwesomeIcon icon={faQuoteLeft} fixedWidth />
-							</div>
-
-							<div className="testimonial-icon-big">
-								<FontAwesomeIcon icon={faQuoteRight} fixedWidth />
-							</div>
-						</div>
-					</div>
-				</div>
-
-				{/* Testimonial 2 */}
-				<div className="col-12">
-					<div className="testimonial-item">
-						<div className="testimonial-content">
-							{/* Testimonial Image */}
-							<div className="testimonial-picture">
-								<StaticImage
-									src="../images/TomMcGinnis.jpg"
-									alt="Tom McGinnis"
-									className="img-fluid"
-								/>
-							</div>
-
-							{/* Testimonial Text */}
-							<div className="testimonial-text">
-								<p>
-									Matt Murphy is a can’t miss prospect. A five-tool player
-									that’s ready to step into any organization and contribute in
-									many ways. He’s a talented, personable, highly-skilled
-									broadcaster who’s not afraid of hard work. I’ve worked with
-									Matt and see a bright future for him!
-								</p>
-							</div>
-							{/* Author */}
-							<div className="testimonial-author-info">
-								<h5 className="testimonial-author">Tom McGinnis</h5>
-								<p className="testimonial-firm">
-									25-year radio voice of the Philadelphia 76ers
-								</p>
-							</div>
-
-							<div className="testimonial-icon">
-								<FontAwesomeIcon icon={faQuoteLeft} fixedWidth />
-							</div>
-
-							<div className="testimonial-icon-big">
-								<FontAwesomeIcon icon={faQuoteRight} fixedWidth />
-							</div>
-						</div>
-					</div>
-				</div>
+				<Testimonial
+					testimonialText="Matt Murphy is a can’t miss prospect. A five-tool player
+          that’s ready to step into any organization and contribute in
+          many ways. He’s a talented, personable, highly-skilled
+          broadcaster who’s not afraid of hard work. I’ve worked with
+          Matt and see a bright future for him!"
+					testimonialAuthor="Tom McGinnis"
+					testimonialFirm="25-year radio voice of the Philadelphia 76ers"
+					imgPath={TomMcGinnis}
+					imgAlt="Tom McGinnis"
+				/>
 			</div>
 		</div>
 	</div>
